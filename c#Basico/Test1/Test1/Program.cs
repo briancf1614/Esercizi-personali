0f76cbfc8d753c21b5d521795a8be7afd3a6912e
brian <brian.potosiedu.itspiemonte.it>
2023-02-20 22:33:43 +0100

    creo que se borrara este ejemplo

---
+++ c#Basico/Test1/Test1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+            Console.WriteLine("este es m{0}i numero,", x);
+            Console.ReadLine();
+        }
+    }
+}
