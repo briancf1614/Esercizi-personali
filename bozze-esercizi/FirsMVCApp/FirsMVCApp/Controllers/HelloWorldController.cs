78cd054828d8295cefe3f656232d06ab8425e0f3
Brian <brian_xpg@hotmail.com>
2023-02-27 20:24:11 +0100

    lala

---
+++ bozze-esercizi/FirsMVCApp/FirsMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using FirsMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirsMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            {
+                Name = "Sif",
+                Age = 2
+            };
+            return View(doggo);
+        }
+
+        public string Hello()
+        {
+            return "who's there?";
+        }
+    }
+}
