59eb2fe18fc82fcde9ab43898dcffbf15884bf0c
Brian Junior Potosi Ccuno <brian.potosi@edu.itspiemonte.it>
2023-03-01 16:25:43 +0100

    Esempio usando razor e bootstrap

---
+++ bozze-esercizi/AplicacionNetRazor/AplicacionNetRazor/Models/Curso.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AplicacionNetRazor.Models
+{
+    public class Curso
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nombre de Curso")]
+        
+        public string NombreCurso { get; set; }
+        [Display(Name = "Cantidad de clases")]
+        public int CantidadClases { get; set; }
+        public int Precio { get; set; }
+        [Display(Name = "Fecha de Creacion")]
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
