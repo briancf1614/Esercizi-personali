c071859b836cb147f603ca64a63373d38caec2fd
BrianPotosi <brian_xpg@hotmail.com>
2025-09-13 10:49:45 +0200

    avanzamento di app, manca il hilt

--- Android_Studio/Curso kotlin AristiDevs/HoroscApp/app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
+++ Android_Studio/Curso kotlin AristiDevs/HoroscApp/app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,5 +25,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initUI();
+    }
+
+    private fun initUI() {
+        initNavigation();
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
---
+++ Android_Studio/Curso kotlin AristiDevs/HoroscApp/app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.horoscapp.databinding.FragmentHorosBinding
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHorosBinding? = null;
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHorosBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
