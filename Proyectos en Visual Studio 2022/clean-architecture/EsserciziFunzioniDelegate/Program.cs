874963786b3491961b365f6a0596825d7f085c54
BRIPOT <b.potosi@syncpoint.info>
2025-12-30 22:31:10 +0100

    esrcizio programmazione declarativa e funzioni delegate

---
+++ Proyectos en Visual Studio 2022/clean-architecture/EsserciziFunzioniDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿List<double> preciosSinIva = new List<double> { 10.0, 20.0, 50.0 };
+
+var a = preciosSinIva.Select(precios => precios + CalcularIva(precios)).ToList();
+
+// pura perche non modifico niente del esterno?
+double CalcularIva(double precio)
+{
+    return precio * 0.21;
+}
+
+
+Predicate<string> utenteValido = nome => nome.Length > 3;
+
+Func<string, string> generatoreToken = nome => "TOKEN-" + nome;
+
+Action<string> LogAccesso = token => Console.WriteLine($"Acceso concedido. Su token es: {token}");
+
+
+if (utenteValido("Mario"))
+{
+    string token = generatoreToken("Mario");
+    LogAccesso(token);
+}
+
+ static string ProcesadorDeTexto(string texto, Func<string, string> operacion)
+{
+    return operacion(texto);
+}
+var grito = ProcesadorDeTexto("hola", texto => texto.ToUpper());
+
+var limpieza = ProcesadorDeTexto("    muchos espacios     ", t => t.Trim());
+
+// esemplare di programmazione IMPERATIVA in quanto non gli sto dicendo io di ciclare o fare a manina logica
